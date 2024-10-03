db752e5cc9111eaea77180da57a74bfaba083e35
Daniela Amariz <damarizt@ufpso.edu.co>
2024-10-03 17:43:21 -0500

    Arreglo

--- src/libs/createAdmin.js
+++ src/libs/createAdmin.js
@@ -1,13 +1,17 @@
 import { user } from '../models/user.model.js'
+import bcrypt from 'bcrypt';
 
 export const createAdmin = async () => {
     try {
       const adminFound = await user.findOne();
       if (adminFound) return;
-  
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash("user1234", salt);
+
       const adminRegistered = await user.create({
         username: "user1234",
-        password: "user1234",
+        password: hashedPassword,
       });
       
       console.log("ID: " + adminFound.username);
