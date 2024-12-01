7cad6dee268c449ab132bdc95dfea10dc98cea95
Vale <vvergelv@ufpso.edu.co>
2024-11-30 20:22:59 -0500

    Actualizado

--- src/config/server.js
+++ src/config/server.js
@@ -42,6 +42,8 @@ export default class Server {
         this.app.use(
             cors({
                 origin: "http://localhost:5173",
+                methods: ['GET', 'POST', 'PUT', 'DELETE'],
+                allowedHeaders: ['Content-Type', 'Authorization']
             })
         );
 
